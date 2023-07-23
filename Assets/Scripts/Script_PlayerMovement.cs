1472c77584fdcd8ecfdc57479d36787f233c9cb6
ConnectedPortal <97449216+ConnectedPortal@users.noreply.github.com>
2023-07-23 13:12:16 +0100

    Camera Control and Capsize Flipping

--- Assets/Scripts/Script_PlayerMovement.cs
+++ Assets/Scripts/Script_PlayerMovement.cs
@@ -7,28 +7,79 @@ public class Script_PlayerMovement : MonoBehaviour
     [Header("References")]
     [SerializeField] private Rigidbody rb;
 
+
     [Header("Movement Values")]
     private Vector3 moveInput;
+    private Vector3 moveForce;
     [SerializeField] private float speed;
-    /*
     [SerializeField] private float maxSpeed;
-    [SerializeField] private float acceleration;
-    [SerializeField] private float maxAcceleration;
-    */
+    [SerializeField] private float drag;
+    [SerializeField] private float steerStrength;
+    [SerializeField] protected bool isCapsized;
+
+    private void Update()
+    {
+        CapsizeDetect();
+        CapsizeFlip();
+    }
 
     private void FixedUpdate()
     {
+        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        PlayerVelocity();
         PlayerSteering();
+    }
+
+    private void PlayerVelocity()
+    {
+        moveForce += transform.forward * speed * moveInput.z * Time.deltaTime;
+        transform.position += moveForce * Time.deltaTime;
+
+        moveForce *= drag;
+        moveForce = Vector3.ClampMagnitude(moveForce, maxSpeed);
     }
 
     private void PlayerSteering()
     {
-        moveInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-
-        Vector3 movement = transform.TransformDirection(moveInput) * speed;
-        rb.velocity = new Vector3(movement.x, rb.velocity.y, movement.z);
+        float steerInput = moveInput.x;
+        transform.Rotate(Vector3.up * steerInput * steerStrength * Time.deltaTime);
     }
 
+    private void CapsizeDetect()
+    {
+        RaycastHit hit;
 
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            if (hit.transform.gameObject.layer != LayerMask.NameToLayer("Water"))
+            {
+                Invoke("CapsizedBool", 2f);
+            }
+        }
+    }
 
+    private void CapsizedBool()
+    {
+        isCapsized = true;
+    }
+
+    private void CapsizeFlip()
+    {
+        if (isCapsized)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this.transform.rotation = Quaternion.identity;
+                isCapsized = false;
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 direction = transform.TransformDirection(-Vector3.up) * 5;
+        Gizmos.DrawRay(transform.position, direction);
+    }
 }
